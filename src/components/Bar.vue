c11e7423d2f950e380150f16e930efd1ab582112
Justin Leniger <jtleniger@gmail.com>
2021-01-14 13:40:58 -0700

    initial commit

---
+++ src/components/Bar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Bar'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Bar/>
+  </div>
+</template>
+
+<script>
+import Bar from '@/components/Bar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Bar
+  }
+}
+</script>
