6361524af8620050cdcbb899423c42a5b854dc4e
Justin Leniger <jtleniger@gmail.com>
2021-01-15 16:13:04 -0700

    Added missing file

---
+++ src/common.js
@@ -0,0 +1,15 @@
+export const SEGMENT_COLORS = [
+  '#7d53deff',
+  '#00c2d1ff',
+  '#ff6b6bff',
+  '#fcff6cff',
+  '#e7bb41ff'
+];
+
+export const INITIAL_PERCENTS = [
+  20,
+  20,
+  20,
+  20,
+  20
+];
