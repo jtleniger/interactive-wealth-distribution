d15c694001d25e9ad6fdbfef52b7b462044e0012
Justin Leniger <jtleniger@gmail.com>
2021-01-15 14:27:35 -0700

    added files

---
+++ src/views/Compare.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div>
+      {{ JSON.stringify(this.$store.state.saved) }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Compare',
+}
+</script>
