6c49a44f928913714cbf1d85d08c0b4e6d391737
Justin Leniger <jtleniger@gmail.com>
2021-01-16 17:13:28 -0700

    creds and write to firestore

--- functions/record.js
+++ functions/record.js
@@ -1,6 +1,18 @@
-exports.handler = async function(event, context) {
-  console.log(`Event: ${JSON.stringify(event)}`);
-  console.log(`Context: ${JSON.stringify(context)}`);
+const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(process.env.GCP_CREDENTIALS);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+exports.handler = async function(event) {
+  const { current, ideal } = JSON.parse(event.body);
+
+  await db.collection('current').add(current);
+  await db.collection('ideal').add(ideal);
 
   return {
       statusCode: 200
