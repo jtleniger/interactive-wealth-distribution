df47d094fb4ec85266d8bf89a276575971b8f324
Justin Leniger <jtleniger@gmail.com>
2021-01-17 18:11:35 -0700

    function aggregates responses

--- functions/record.js
+++ functions/record.js
@@ -9,13 +9,48 @@ admin.initializeApp({
 const db = admin.firestore();
 
 exports.handler = async function(event) {
-  const { current, ideal } = JSON.parse(event.body);
+  const { current: newCurrentData, ideal: newIdealData } = JSON.parse(event.body);
 
-  console.log(current);
-  console.log(ideal);
+  const dataRef = db.collection('data');
 
-  await db.collection('current').add({ current });
-  await db.collection('ideal').add({ ideal });
+  const currentDoc = await dataRef.doc('current').get();
+  const idealDoc = await dataRef.doc('ideal').get();
+
+  if (!currentDoc.exists || !idealDoc.exists) {
+    if (!currentDoc.exists) {
+      await currentDoc.set({
+        count: 1,
+        values: newCurrentData
+      });
+    }
+  
+    if (!idealDoc.exists) {
+      await idealDoc.set({
+        count: 1,
+        values: newIdealData
+      });
+    }
+
+    return {
+      statusCode: 200
+    };
+  }
+
+  let oldCurrentData = currentDoc.data();
+  let oldIdealData = idealDoc.data();
+
+  oldCurrentData.count++;
+  oldIdealData.count++;
+
+  await currentDoc.set({
+    count: oldCurrentData.count,
+    values: oldCurrentData.map((val, idx) => val + ((newCurrentData[idx] - val) / oldCurrentData.count))
+  })
+
+  await idealDoc.set({
+    count: oldIdealData.count,
+    values: oldIdealData.map((val, idx) => val + ((newIdealData[idx] - val) / oldIdealData.count))
+  })
 
   return {
       statusCode: 200
